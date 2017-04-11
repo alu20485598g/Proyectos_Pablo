3259fcf7881bcd20410f764d1d381f594dc92549
pablo <pablo@pablo-VirtualBox>
2017-04-11 12:45:15 +0200

    Practica76

--- Practica76/src/practica76/Calculadora.java
+++ Practica76/src/practica76/Calculadora.java
@@ -6,19 +6,40 @@
 package practica76;
 
 /**
- *
+ * Clase Calculadora. Encontramos todos los métodos para realizar las 
+ * diferentes operaciones.
  * @author pablo
  */
 public class Calculadora {
+    /** Método que devuelve la suma de num1 y num2
+     * @param num1 Variable donde encontramos el valor del primer número.
+     * @param num2 Variable donde encontramos el valor del segundo número.
+     * @return Devolverá un número de tipo float como resultado 
+     * de la operación suma.*/
     public float suma(float num1, float num2) {
 		return num1 + num2;
-	}     
+	}
+     /** Método que devuelve la resta de num1 y num2
+     * @param num1 Variable donde encontramos el valor del primer número.
+     * @param num2 Variable donde encontramos el valor del segundo número.
+     * @return Devolverá un número de tipo float como resultado 
+     * de la operación resta.*/
     public float resta(float num1, float num2) {
-		return num1 - num2;
-	}     
+		return num1 - num2;  
+	}
+    /** Método que devuelve la multiplicación de num1 y num2
+     * @param num1 Variable donde encontramos el valor del primer número.
+     * @param num2 Variable donde encontramos el valor del segundo número.
+     * @return Devolverá un número de tipo float como resultado 
+     * de la operación multiplicación.*/
     public float multi(float num1, float num2) {
 		return num1 * num2;
-	}     
+        }
+    /** Método que devuelve la división de num1 y num2
+     * @param num1 Variable donde encontramos el valor del primer número.
+     * @param num2 Variable donde encontramos el valor del segundo número.
+     * @return Devolverá un número de tipo float como resultado 
+     * de la operación división.*/
     public float divi(float num1, float num2) {
 		return num1 / num2;
 	}
--- Practica76/src/practica76/Practica76.java
+++ Practica76/src/practica76/Practica76.java
@@ -8,20 +8,31 @@ package practica76;
 import java.util.Scanner;
 
 /**
- *
+ * Clase principal. Encontraremos el diseño del menú y de las diferentes
+ * opciones que contará nuestra calculadora.
  * @author pablo
  */
 public class Practica76 {
 
     /**
+     * 
      * @param args the command line arguments
+     * 
      */
     public static void main(String[] args) {
+    /* Declaramos las variables aux, num1, num2, resul */
+    /** @param aux Variable para llevar el control de la opción del menú.
+     * @param num1 Variable para guardar el valor del primer número.
+     * @param num2 Variable para guardar el valor del segundo número.
+     * @param resul Variable para guardar el valor del relsultado de la operación.
+     */
     int aux;
     float num1;
     float num2;
     float resul;
+    /* Hacemos llamada a la clase calculadora mediante el objeto c.*/
     Calculadora c = new Calculadora();
+    /* Creamos un menú para que el usuario nos devuelva la opción deseada. */ 
     Scanner teclado = new Scanner(System.in);
     System.out.println("Dime que operación quieres realizar:");
     System.out.println("1.Sumar");
@@ -36,31 +47,42 @@ public class Practica76 {
         System.out.println("No me has escogido una opción correcta.");
     }    
     else{
-        
+        /* Creamos un bucle while hasta que el usuario nos devuelva la 
+        opción 5 que es la de salir de la calculadora. */
         while (aux > 0 && aux < 5){
+            /* Pedimos al usuario que nos de un valor para num1 y num2 */
             Scanner teclado3 = new Scanner(System.in);
             System.out.print("Dime el primer número: ");
             num1=teclado3.nextFloat();
             Scanner teclado4 = new Scanner(System.in);
             System.out.print("Dime el segundo número: ");
             num2=teclado4.nextFloat();
+            /* Con el switch podremos realizar una operacion diferente según la 
+            opción que el usuario ha introducido previamente */
             switch (aux){
                 case 1:
+                    /* A través del objeto podremos hacer una llamada al método
+                    suma de la clase calculadora pasandole los valores de num1 
+                    y num2, guardando asi el resultado que nos devuelva el 
+                    método en la variable resul. */
                     resul = c.suma(num1, num2);                    
                     System.out.println("Resultado: "+ resul);
                     System.out.println();
                 break;
-                case 2: 
+                case 2:
+                    /* Igual que para la suma tendremos el método resta. */
                     resul = c.resta(num1, num2);                    
                     System.out.println("Resultado: "+ resul);
                     System.out.println();
                 break;
-                case 3: 
+                case 3:
+                    /* Método multi. */
                     resul = c.multi(num1, num2);                    
                     System.out.println("Resultado: "+ resul);
                     System.out.println();
                 break;
                 case 4: 
+                    /* Método divi. */
                     resul = c.divi(num1, num2);                    
                     System.out.println("Resultado: "+ resul);
                     System.out.println();
