0dceaf266691da954efd285c20fea9edaea4cd17
pablo <pablo@pablo-VirtualBox>
2017-04-25 12:36:54 +0200

    Practica76

--- Practica77/src/practica77/Practica77.java
+++ Practica77/src/practica77/Practica77.java
@@ -8,7 +8,9 @@ package practica77;
 import java.util.Scanner;
 
 /**
- *
+ * Clase principal. Encontraremos el acceso a las diferentes clases las cuales 
+ * realizarán las operaciones para que nos devuelva el resultado correcto y 
+ * poder mostrarlo.
  * @author pablo
  */
 public class Practica77 {
@@ -20,12 +22,17 @@ public class Practica77 {
     float r;
     float resull;
     float resula;
+    /*Hacemos la llamada a la clase areayl en la cual calcularemos el área y 
+    perímetro*/
     areayl ayl = new areayl();
     Scanner teclado = new Scanner (System.in);
     System.out.println("Dame el radio: ");
     r = teclado.nextFloat();
+    /*En las variables resull y resula guardaremos el valor que nos devuelva 
+    los métodos de la clase areyl*/
     resull = ayl.getlongitud(r);
     resula = ayl.getarea(r);
+    /*Mostramos por pantalla los resultados*/
     System.out.println("El área es "+ resula +" m2");
     System.out.println("El perímetro es "+ resull +" m");
     }  
--- Practica77/src/practica77/areayl.java
+++ Practica77/src/practica77/areayl.java
@@ -6,17 +6,26 @@
 package practica77;
 
 /**
- *
+ * Clase areayl. Encontramos todos los métodos para realizar las 
+ * operaciones para calcular el perímetro y el área.
  * @author pablo
  */
 public class areayl {
-     private float resul;   
-
+/** Método que devuelve el perímetro de la circunferencia
+* @param resul Variable donde encontramos el el valor del perímetro.
+* @param r Variable que coje de la clase principal y donde encontramos el radio
+* pasado por el usuario.
+* @return Devolverá un número de tipo float con el valor del perímetro.*/
+  private float resul;
 public float getlongitud(float r){
     resul=(float) (r*2*Math.PI);
     return resul;
 }
-
+/** Método que devuelve el área de la circunferencia
+* @param resul Variable donde encontramos el el valor del área.
+* @param r Variable que coje de la clase principal y donde encontramos el radio
+* pasado por el usuario.
+* @return Devolverá un número de tipo float con el valor del área.*/
 public float getarea(float r){
     resul=(float) (Math.pow(r, 2)*Math.PI);
     return resul;
