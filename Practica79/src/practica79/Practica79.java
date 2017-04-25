1878678e839f7c7e6f56cbdc0f183ac5a955817e
pablo <pablo@pablo-VirtualBox>
2017-04-25 13:28:07 +0200

    Practica79

--- Practica79/src/practica79/Practica79.java
+++ Practica79/src/practica79/Practica79.java
@@ -8,7 +8,9 @@ package practica79;
 import java.util.Scanner;
 
 /**
- *
+ * Clase principal. Encontraremos el acceso a la clase areap la cual realizará
+ * las operaciones para que nos devuelva el resultado correcto y poder 
+ * mostrarlo.
  * @author pablo
  */
 public class Practica79 {
@@ -20,15 +22,19 @@ public class Practica79 {
     /*Declaración de variables*/
     float r;
     float l;
+    /*Hacemos la llamada a la clase areap en la cual calcularemos el área 
+    pentágono.*/
     areap ap = new areap();
-    /*Entrada por teclado, pedimos lel radio y el lado al usuario y adjudicamos los valores a las variables*/
+    /*Entrada por teclado, pedimos lel radio y el lado al usuario y adjudicamos
+    los valores a las variables*/
     Scanner teclado = new Scanner (System.in);
     System.out.println("Dame el radio: ");
     r = teclado.nextFloat();
     Scanner teclado2 = new Scanner (System.in);
     System.out.println("Dame el lado: ");
     l = teclado2.nextFloat();
-    /*Mostramos los resultados de las operaciones*/
+    /*Mostramos el valor del área que nos devolverá el método getarea de la 
+    clase areap*/
     System.out.println("El área es "+ ap.getarea(r, l) +" m2");
         }
     
--- Practica79/src/practica79/areap.java
+++ Practica79/src/practica79/areap.java
@@ -6,13 +6,20 @@
 package practica79;
 
 /**
- *
+ * Clase areap. Encontramos el método getarea el cual nos permitirá calcular el 
+ * área del pentágono.
  * @author pablo
  */
 public class areap {
     private float resul;
     private float apotema;
     private float perimetro;
+    /** Método que devuelve el perímetro de la circunferencia
+* @param r Variable que coje de la clase principal y donde encontramos el radio 
+* del pentágono pasado por el usuario.
+* @param l Variable que coje de la clase principal y donde encontramos el lado 
+* del pentágono pasado por el usuario.
+* @return Devolverá un número de tipo float con el valor del área(resul).*/
 public float getarea(float r, float l){
     apotema = (float) Math.sqrt(Math.pow(r,2) - Math.pow((l/2),2));
     perimetro = l*5;
