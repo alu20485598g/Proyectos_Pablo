ef622c05343a80208179199e45568cf8b4f9c51f
pablo <pablo@pablo-VirtualBox>
2017-04-25 13:04:09 +0200

    Practica78

--- Practica78/src/practica78/Practica78.java
+++ Practica78/src/practica78/Practica78.java
@@ -8,7 +8,9 @@ package practica78;
 import java.util.Scanner;
 
 /**
- *
+ * Clase principal. Encontraremos el acceso a la clase areat la cual realizará
+ * las operaciones para que nos devuelva el resultado correcto y poder 
+ * mostrarlo.
  * @author pablo
  */
 public class Practica78 {
@@ -20,14 +22,19 @@ public class Practica78 {
     float h;
     float b;
     float resula;
+    /*Hacemos la llamada a la clase areat en la cual calcularemos el área.*/
     areat at = new areat();
+    /*Pedimos la base y la altura al usuario.*/
     Scanner teclado = new Scanner (System.in);
     System.out.println("Dame la base: ");
     b = teclado.nextFloat();
     Scanner teclado2 = new Scanner (System.in);
     System.out.println("Dame la altura: ");
     h = teclado.nextFloat();
+    /*En la variable resula guardaremos el valor que nos devuelva el método
+    de la clase areat*/
     resula= at.getarea(b, h);
+    /*Mostramos por pantalla los resultados*/
     System.out.println("El área es "+ resula +" m2");
     }
     
--- Practica78/src/practica78/areat.java
+++ Practica78/src/practica78/areat.java
@@ -6,12 +6,18 @@
 package practica78;
 
 /**
- *
+ * Clase areat. Encontramos el método getarea el cual nos permitirá calcular el 
+ * área del triángulo.
  * @author pablo
  */
 public class areat {
     private float resul;
-    
+    /** Método que devuelve el perímetro de la circunferencia
+* @param b Variable que coje de la clase principal y donde encontramos la base 
+* del triángulo pasado por el usuario.
+* @param h Variable que coje de la clase principal y donde encontramos la altura 
+* del triángulo pasado por el usuario.
+* @return Devolverá un número de tipo float con el valor del perímetro(resul).*/
 public float getarea(float b, float h){
     resul=h*b/2;
     return resul;
