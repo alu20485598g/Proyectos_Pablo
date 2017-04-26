4cb610d8e7e393a70d655755e94b3c9d4b658133
pablo <pablo@pablo-VirtualBox>
2017-04-26 09:57:05 +0200

    Practica81

--- Practica81/src/practica81/Practica81.java
+++ Practica81/src/practica81/Practica81.java
@@ -8,7 +8,9 @@ package practica81;
 import java.util.Scanner;
 
 /**
- *
+ * Clase principal. Encontraremos el acceso a la clase ayve la cual realizará
+ * las operaciones para que nos devuelva el valor correcto del área y volumen 
+ * de la esfera y poder mostrarlo.
  * @author pablo
  */
 public class Practica81 {
@@ -19,12 +21,15 @@ public class Practica81 {
     public static void main(String[] args) {
     /*Declaración de variables*/
     float r;
+     /*Hacemos la llamada a la clase ayve en la cual calcularemos el área y 
+    volumen del cubo.*/
     ayve e = new ayve();
     /*Entrada por teclado, pedimos el radio al usuario y adjudicamos los valores a las variables*/
     Scanner teclado = new Scanner (System.in);
     System.out.println("Dame el radio: ");
     r = teclado.nextFloat();
-    /*Mostramos los resultados de las operaciones*/
+    /*Mostramos el valor del área que nos devolverá el método getarea y 
+    getvolumen de la clase ayve*/
     System.out.println("El área es "+ e.getarea(r) +" m2");
     System.out.println("El volumen es "+ e.getvolumen(r) +" m3");
     }
