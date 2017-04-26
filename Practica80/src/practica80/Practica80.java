88c6b937999d62bcebe3551e847b2a87f20c42ed
pablo <pablo@pablo-VirtualBox>
2017-04-26 08:53:43 +0200

    Practica80

--- Practica80/src/practica80/Practica80.java
+++ Practica80/src/practica80/Practica80.java
@@ -8,7 +8,9 @@ package practica80;
 import java.util.Scanner;
 
 /**
- *
+ * Clase principal. Encontraremos el acceso a la clase areac la cual realizará
+ * las operaciones para que nos devuelva el valor correcto del área del círculo
+ * y poder mostrarlo.
  * @author pablo
  */
 public class Practica80 {
@@ -17,11 +19,18 @@ public class Practica80 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    /* Declaración de variable l (lado)*/
     float l;
+     /*Hacemos la llamada a la clase areap en la cual calcularemos el área 
+    círculo.*/
     areac ac = new areac();
+    /*Entrada por teclado, pedimos del lado al usuario y adjudicamos
+    los valores a las variables*/
     Scanner teclado = new Scanner (System.in);
     System.out.println("Dame el radio: ");
     l = teclado.nextFloat();
+    /*Mostramos el valor del área que nos devolverá el método getareal y 
+    getareat de la clase areac*/
     System.out.println("El área lateral es "+ ac.getareal(l) +" m2");
     System.out.println("El área total es "+ ac.getareat(l) +" m2");
     }
--- Practica80/src/practica80/areac.java
+++ Practica80/src/practica80/areac.java
@@ -6,16 +6,24 @@
 package practica80;
 
 /**
- *
+ * Clase areap. Encontramos el método getareal y getareat el cual nos permitirá 
+ * calcular el área del círculo.
  * @author pablo
  */
 public class areac {
     private float resul;
-    
+     /** Método que devuelve el perímetro de la circunferencia
+* @param l Variable que coje de la clase principal y donde encontramos el lado 
+* del círculo pasado por el usuario.
+* @return Devolverá un número de tipo float con el valor del área(resul).*/
 public float getareal(float l){
     resul=l*l*4;
     return resul;
 }
+/** Método que devuelve el perímetro de la circunferencia
+* @param l Variable que coje de la clase principal y donde encontramos el lado 
+* del círculo pasado por el usuario.
+* @return Devolverá un número de tipo float con el valor del área(resul).*/
 public float getareat(float l){
     resul=l*l*6;
     return resul;
