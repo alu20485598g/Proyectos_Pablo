cd51fa62e09602d5f5c6acf542c3caab98e70419
pablo <pablo@pablo-VirtualBox>
2017-04-28 08:57:45 +0200

    Practica86

--- Practica86/src/practica86/Distancia.java
+++ Practica86/src/practica86/Distancia.java
@@ -8,7 +8,8 @@ package practica86;
 import java.util.Scanner;
 
 /**
- *
+ * Clase Distancia. Encontramos diferentes métodos los cuales nos permitirá 
+ * pedir los pies al usuario y convertir los pies al resto de unidades.
  * @author pablo
  */
 public class Distancia {
@@ -17,18 +18,26 @@ public class Distancia {
     private float pulgadas;
     private float metros;
     private float cm;
-    
+    /**Método que pide al usuario los pies a converir.
+     * 
+     */
     public void PedirPies(){
         System.out.print("Dime cuantos pies quieres convertir: ");
         Scanner teclado = new Scanner(System.in);
         pies=teclado.nextInt();
     }
+    /**Método que convierte los pies en el resto de unidades.
+     * 
+     */
     public void Convertir(){
         yardas = pies/3;
         pulgadas = pies*12;
         metros = (float) (pies*0.3048);
         cm = (float) (pies*30.48);
     }
+    /**Método que devuelve las unidades convertidas.
+     * 
+     */
     public void GetConvertir(){
         System.out.println("-----------------------------------");
         System.out.println("Los piess introducidos equivalen a:");
--- Practica86/src/practica86/Practica86.java
+++ Practica86/src/practica86/Practica86.java
@@ -6,7 +6,9 @@
 package practica86;
 
 /**
- *
+ * Clase principal. En esta clase encontraremos las diferentes llamadas a la 
+ * clase Distancia, la cual será la encargada de realizar las diferentes 
+ * funciones a través de sus métodos.
  * @author pablo
  */
 public class Practica86 {
@@ -16,9 +18,13 @@ public class Practica86 {
      */
     public static void main(String[] args) {
     float pies;
+    /*Llamamos a la nueva clase Distancia a través del objeto d1.*/
     Distancia d1 = new Distancia();
+    /*Con este método pedimos los pies a convertir al usuario.*/
     d1.PedirPies();
+    /*Con este método convertimos los pies al resto de unidades.*/
     d1.Convertir();
+    /*Con este método devolvemos las medidas convertidas.*/
     d1.GetConvertir();
     }
     
