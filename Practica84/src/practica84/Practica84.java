9b1eeba629ba0826af52ce143246a921213a7ab1
pablo <pablo@pablo-VirtualBox>
2017-04-28 08:57:09 +0200

    Practica84

--- Practica84/src/practica84/Practica84.java
+++ Practica84/src/practica84/Practica84.java
@@ -6,7 +6,10 @@
 package practica84;
 
 /**
- *
+ * Clase principal. En esta clase encontraremos las diferentes llamadas a la 
+ * clase pitagoras, la cual será la encargada de realizar las diferentes 
+ * funciones a través de sus métodos. En esta clase imprimiremos el resultado 
+ * final.
  * @author pablo
  */
 public class Practica84 {
@@ -15,10 +18,15 @@ public class Practica84 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+ /*Llamamos a la nueva clase pitagoras a través del objeto p1.*/
     pitagoras p1 = new pitagoras();
+    /*Con este método pedimos el primer cateto al usuario.*/
     p1.pedircateto1();
+    /*Con este método pedimos el segundo cateto al usuario.*/
     p1.pedircateto2();
+    /*Con este método calculamos la hipotenusa.*/
     p1.calculohipo();
+    /*Mostramos por pantalla los resultados devueltos por el método getrhipo.*/
     System.out.println("La hipotenusa es igual a: "+ p1.gethipo());
     }
     
--- Practica84/src/practica84/pitagoras.java
+++ Practica84/src/practica84/pitagoras.java
@@ -8,28 +8,39 @@ package practica84;
 import java.util.Scanner;
 
 /**
- *
+ * Clase pitagoras. Encontramos diferentes métodos los cuales nos permitirá 
+ * pedir los diferentes catetos y calcular la hipotenusa.
  * @author pablo
  */
 public class pitagoras {
     private float cateto1;
     private float cateto2;
     private float hipo;
-    
+   /**Método que pide al usuario el primer cateto.
+     * 
+     */
     public void pedircateto1(){
         Scanner teclado = new Scanner(System.in);
         System.out.print("Dame el primer cateto: ");
         cateto1 = teclado.nextFloat();
     }
+    /**Método que pide al usuario el segundo cateto.
+     * 
+     */
     public void pedircateto2(){
         Scanner teclado2 = new Scanner(System.in);
         System.out.print("Dame el segundo cateto: ");
         cateto2 = teclado2.nextFloat();
     }
+    /**Método que calcula la hipotenusa del triángulo.
+     * 
+     */
     public void calculohipo(){
         hipo = (float) Math.sqrt(Math.pow(cateto1,2) + Math.pow(cateto2, 2));
     }
-    
+    /**Método que devuelve el valor de la hipotenusa del triángulo.
+     * 
+     */
     public float gethipo(){
         return hipo;
     }
