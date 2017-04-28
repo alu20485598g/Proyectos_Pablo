891bb15d8ccdf8c6dd0d1a95654717d44c5bd2f0
pablo <pablo@pablo-VirtualBox>
2017-04-28 08:56:49 +0200

    Practica83

--- Practica83/src/practica83/Practica83.java
+++ Practica83/src/practica83/Practica83.java
@@ -6,7 +6,10 @@
 package practica83;
 
 /**
- *
+ * Clase principal. En esta clase encontraremos las diferentes llamadas a la 
+ * clase ecuacion2, la cual será la encargada de realizar las diferentes 
+ * funciones a través de sus métodos. En esta clase imprimiremos el resultado 
+ * final.
  * @author pablo
  */
 public class Practica83 {
@@ -15,13 +18,22 @@ public class Practica83 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+   /*Llamamos a la nueva clase ecuacion2 a través del objeto ec.*/
     ecuacion2 ec = new ecuacion2();
+    /*Con este método pedimos la variable a de la ecuación al usuario.*/
     ec.pedira();
+    /*Con este método pedimos la variable b de la ecuación al usuario.*/
     ec.pedirb();
+    /*Con este método pedimos la variable c de la ecuación al usuario.*/
     ec.pedirc();
+    /*Con este método calculamos la parte de la raíz de la ecuación.*/
     ec.calculoraiz();
+    /*Con este método calculamos el primer resultado de la ecuación.*/
     ec.resultado1();
+    /*Con este método calculamos el segundo resultado de la ecuación.*/
     ec.resultado2();
+    /*Mostramos por pantalla los resultados devueltos por los métodos 
+    getresultado1 y getresultado2.*/
     System.out.println("Los dos resultados de la ecuación de 2º grado son: ");
     System.out.print("Resultado 1: "+ ec.getresultado1());
     System.out.println("");
--- Practica83/src/practica83/ecuacion2.java
+++ Practica83/src/practica83/ecuacion2.java
@@ -8,7 +8,8 @@ package practica83;
 import java.util.Scanner;
 
 /**
- *
+ * Clase ecuacion2. Encontramos diferentes métodos los cuales nos permitirá 
+ * pedir las diferentes variables y resolver la ecuación.
  * @author pablo
  */
 public class ecuacion2 {
@@ -18,40 +19,58 @@ public class ecuacion2 {
     private float b;
     private float c;
     private float raiz;
-    
+    /**Método que pide al usuario la variable a.
+     * 
+     */
     public void pedira(){
         Scanner teclado = new Scanner(System.in);
         System.out.print("Dame a(x2): ");
         a = teclado.nextFloat();
     }
-    
+    /**Método que pide al usuario la variable b.
+     * 
+     */
     public void pedirb(){
         Scanner teclado2 = new Scanner(System.in);
         System.out.print("Dame b(x): ");
         b = teclado2.nextFloat();
     }
-    
+    /**Método que pide al usuario la variable c.
+     * 
+     */
     public void pedirc(){
         Scanner teclado3 = new Scanner(System.in);
         System.out.print("Dame c(número): ");
         c = teclado3.nextFloat();
     }
-    
+    /**Método que calcula la parte de la raíz de la ecuación de 2º grado.
+     * 
+     */
     public void calculoraiz(){
         raiz = (float) Math.sqrt(Math.pow(b , 2) + (-4 * a * c ));
     }
-    
+    /**Método que calcula el primer resultado de la ecuación.
+     * 
+     */
     public void resultado1(){
         resultado1 = (-b + raiz) / (2 * a);        
     }
+    /**Método que devuelve el primer resultado de la ecuación.
+     * 
+     */
     public float getresultado1(){
         return resultado1;
     }
-    
+    /**Método que calcula el segundo resultado de la ecuación.
+     * 
+     */
     public void resultado2(){
         resultado2 = (-b - raiz) / (2 * a);
         
     }
+    /**Método que devuelve el segundo resultado de la ecuación.
+     * 
+     */
     public float getresultado2(){
         return resultado2;
     }
