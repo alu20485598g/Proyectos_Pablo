0ba05733776ab904cdae28bf26b85afd3b280eca
pablo <pablo@pablo-VirtualBox>
2017-05-24 08:46:26 +0200

    Practica85_2 (Ejercicio Cuentas ArrayList)

--- Practica85_2/src/practica85_2/cuenta.java
+++ Practica85_2/src/practica85_2/cuenta.java
@@ -39,47 +39,87 @@ public class cuenta {
     public cuenta(cuenta c1) {
         this.tipo_interes=c1.tipo_interes;
     }
-    /*Getters y Setters para guardar los datos de cada objeto de la clase*/
+     /**Método set de la variable nombre
+     * @param nombre Variable con el valor de nombre
+     */
      public void setnombre(String nombre){
         this.nombre=nombre;
     }
+     /**Método set de la variable apellido1
+     * @param apellido1 Variable con el valor de apellido1
+     */
       public void setapellido1(String apellido1){
         this.apellido1=apellido1;
     }
+     /**Método set de la variable apellido2
+     * @param apellido2 Variable con el valor de apellido2
+     */
        public void setapellido2(String apellido2){
         this.apellido2=apellido2;
     }
+     /**Método set de la variable DNI
+     * @param DNI Variable con el valor de DNI
+     */
         public void setDNI(String DNI){
         this.DNI=DNI;
     }
+     /**Método set de la variable saldo
+     * @param saldo Variable con el valor de saldo
+     */
     public void setsaldo(float saldo){
         this.saldo=saldo;
     }
+    /**Método set de la variable tipo_interes
+     * @param tipo_interes Variable con el valor de tipo_interes
+     */
     public void setinteres(float tipo_interes){
         this.tipo_interes=tipo_interes;
     }
+    /**Método set de la variable num_cuenta
+     * @param num_cuenta Variable con el valor de num_cuenta
+     */
     public void setncuenta(int num_cuenta){
         this.num_cuenta=num_cuenta;
     }    
-    
+    /**Método get de la variable nombre
+     * @return Devuelve el valor de nombre
+     */
     public String getnombre(){
         return nombre;
     }
+    /**Método get de la variable apellido1
+     * @return Devuelve el valor de apellido1
+     */
     public String getapellido1(){
         return apellido1;
     }
+    /**Método get de la variable apellido2
+     * @return Devuelve el valor de apellido2
+     */
     public String getapellido2(){
         return apellido2;
     }
+    /**Método get de la variable DNI
+     * @return Devuelve el valor de DNI
+     */
     public String getDNI(){
         return DNI;
     }
+    /**Método get de la variable saldo
+     * @return Devuelve el valor de saldo
+     */
     public float getsaldo(){
         return saldo;
     }
+    /**Método get de la variable tipo_interes
+     * @return Devuelve el valor de tipo_interes
+     */
     public float getinteres(){
         return tipo_interes;
     }
+    /**Método get de la variable num_cuenta
+     * @return Devuelve el valor de num_cuenta
+     */
     public int getncuenta(){
         return num_cuenta;
     }
